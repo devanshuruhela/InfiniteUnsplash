84c04aeed5f425c8d392e4cbe0e10225a358f0db
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2021-08-18 02:20:23 +0530

    hidden api key

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
+const imagecontainer = document.getElementById('image_container');
+const loader = document.getElementById('loader');
 
-const apikey = "g8JFlzSvYEA_1eR5hq_8UCLmXA6w7wRKi36nLfFawac";
+let photosarray = [];
+
+const apikey = config.SECRET_API_KEY;
 const count = 10;
 
 const apiurl = `https://api.unsplash.com/photos/random?client_id=${apikey}&count=${count}`;
+
+
+function displayphotos()
+{
+  //running dunction for each object to create and append in dom
+  photosarray.forEach(photo=>
+    {
+      const item = document.createElement('a');
+      item.setAttribute('href' , photo.links.html );
+      item.setAttribute('target' , '_blank')
+
+      // creating image tag
+      const img = document.createElement('img');
+      img.setAttribute('src' , photo.urls.regular);
+      img.setAttribute('title' , photo.alt_description);
+      img.className= "photosize"
+
+      item.appendChild(img);
+      imagecontainer.appendChild(item);
+    });
+}
 
 
 async function fetchphotos()
 {
   try {
     const response = await fetch(apiurl);
-    const data = await response.json();
-    console.log(data);
+    photosarray = await response.json();
+    displayphotos();
   } catch (error) {
     
   }
 }
 
-//fetchphotos();
+fetchphotos();
